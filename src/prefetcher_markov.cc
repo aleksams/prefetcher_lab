ce4ded7304b970f27f04f11972884b868abd58e9
gunnar <gunnar@moon.ai>
2019-03-22 10:36:08 +0100

    tabs to spaces

--- src/prefetcher_markov.cc
+++ src/prefetcher_markov.cc
@@ -7,12 +7,12 @@
 #include "interface.hh"
 
 struct table_input {
-	Addr addr;
-	int count;
+  Addr addr;
+  int count;
 };
 
 const unsigned int MAX_TABLE_ROWS = 64*64;
-const unsigned int MAX_TABLE_COLUMNS = 8;
+const unsigned int MAX_TABLE_COLUMNS = 64*4;
 
 //Addr miss_table[MAX_LIST_SIZE][LIST_INPUTS];
 //Addr miss_table_index[MAX_LIST_SIZE];
@@ -29,77 +29,70 @@ class Table {
 
     void insert_miss (Addr miss)
     {
-	int rows    =    MAX_TABLE_ROWS;
-	int columns = MAX_TABLE_COLUMNS;
-	bool found = false;
+  int rows    =    MAX_TABLE_ROWS;
+  int columns = MAX_TABLE_COLUMNS;
+  bool found = false;
         if (last_miss_index!=-1) {
-	    for (int i=0; i < columns; i++) {
-		if (miss_table[last_miss_index][i].addr == miss){
-			miss_table[last_miss_index][i].count++;
-			found = true; //use this flag to determine that the addr was already in the matrix
-			break;
-		}
-	    }
-	    if (found == false){
-		//Replace miss_table content with new miss.
-		int lowest = 0;
+      for (int i=0; i < columns; i++) {
+  if (miss_table[last_miss_index][i].addr == miss){
+  miss_table[last_miss_index][i].count++;
+  found = true; //use this flag to determine that the addr was already in the matrix
+  break;
+  }
+      }
+      if (found == false){
+  //Replace miss_table content with new miss.
+  int lowest = 0;
                 for (int i=1; i < columns; i++) {
                     if (miss_table[last_miss_index][i].count <= miss_table[last_miss_index][lowest].count){
                         lowest = i;
                     }
                 }
-		miss_table[last_miss_index][lowest].addr = miss;
-		miss_table[last_miss_index][lowest].count = 1;
+  miss_table[last_miss_index][lowest].addr = miss;
+  miss_table[last_miss_index][lowest].count = 1;
   	    }
-	}
-	found = false;
+  }
+  found = false;
         for (int i=0; i<rows; i++) {
-	    if (index_list[i].addr == miss) {
-		index_list[i].count++;
+      if (index_list[i].addr == miss) {
+  index_list[i].count++;
                 last_miss_index = i;
-		found = true;
-		break;
-	    }
-	}
-	if (found == false) {
+  found = true;
+  break;
+      }
+  }
+  if (found == false) {
             int lowest = 0;
             for (int i=1; i < rows; i++) {
                 if (index_list[i].count <= index_list[lowest].count){
                     lowest = i;
                 }
             }
-	    last_miss_index = lowest;
+      last_miss_index = lowest;
             index_list[lowest].addr = miss;
             index_list[lowest].count = 1;
-	    for (int i=0; i<columns;i++) {
-	        miss_table[lowest][i].addr = 0;
+      for (int i=0; i<columns;i++) {
+          miss_table[lowest][i].addr = 0;
                 miss_table[lowest][i].count = 0;
-	    }
-	}
+      }
+  }
     }
 
     Addr get_next_miss (Addr miss) {
-			int rows = MAX_TABLE_ROWS;
-			int columns = MAX_TABLE_COLUMNS;
+  int rows = MAX_TABLE_ROWS;
+  int columns = MAX_TABLE_COLUMNS;
         for (int i=0; i<rows; i++) {
             if (index_list[i].addr == miss) {
-                int num = 0;
-								for (int j=0; j<columns; j++) {
+                int highest = 0;
+  for (int j=1; j<columns; j++) {
                     if (miss_table[i][j].count >= miss_table[i][highest].count){
                         highest = j;
-		    						}
-		    					/*	if (!in_cache(miss_table[i][j].addr) && miss_table[i][j].addr!=0){// && pf_addr<MAX_PHYS_MEM_ADDR){
-                    		issue_prefetch(miss_table[i][j].addr);
-												num++;
-                   }*/
-								}
-							/*if(num==0) {
-		    				issue_prefetch(miss + BLOCK_SIZE);
-							}*/
-	    		return miss_table[i][highest].addr;
-	    			}
+      }
+  }
+      return miss_table[i][highest].addr;
+      }
         }
-				return 0;
+  return 0;
     }
 };
 
@@ -112,7 +105,7 @@ void prefetch_init(void)
     /* Called before any calls to prefetch_access. */
     /* This is the place to initialize data structures. */
 
-		markov_table = new Table;
+  markov_table = new Table;
     DPRINTF(HWPrefetch, "Initialized Markov prefetcher\n");
 }
 
@@ -131,13 +124,14 @@ void prefetch_access(AccessStat stat)
     if (stat.miss) {
         markov_table->insert_miss(stat.mem_addr);
         Addr pf_addr = markov_table->get_next_miss(stat.mem_addr);
-/*	if (pf_addr==0) {
+  /*if (pf_addr==0) {
             pf_addr = stat.mem_addr + BLOCK_SIZE;
-  */      }
-
-/*	if (!in_cache(pf_addr) && pf_addr!=0){// && pf_addr<MAX_PHYS_MEM_ADDR){
-		issue_prefetcher(pf_addr);
-	}*/
+        }*/
+  if (!in_cache(pf_addr) && pf_addr!=0){// && pf_addr<MAX_PHYS_MEM_ADDR){
+      issue_prefetch(pf_addr);
+  }
+    }
+}
 
 void prefetch_complete(Addr addr) {
     /*
